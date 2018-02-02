b6a2dad4dfccbe57c603a1c399b84380c51cac7b
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 19:43:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,15 @@
 class Song
   @@count=0
-  def initialize 
-    @@count += 1 
+  @@genres= []
+  @@artists=
+  def initialize()
+    
+    @@count += 1
   def self.count
     @@count
   end
   def self.artists
   end
   def self.genres
-  end 
+  end
 end
