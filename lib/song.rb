c3968f9a631d824aa330c27ab24a13aba3eeacbd
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 20:23:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,9 @@ class Song
   @@count=0
   @@genres= []
   @@artists= []
-  attr_accessor :name :artist :genre
+  attr_accessor :name
+  attr_accessor :artist 
+  attr_accessor :genre
   def initialize(name,genre,artist)
     @name=name
     @genre=genre
