cc98f06728e1b94432f1505f8cc3e4161335d73d
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 20:05:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,11 +14,10 @@ class Song
     @@count
   end
   def self.artists
-    if @@artists.
-    @@artists
+    @@artists.uniq
   end
   def self.genres
-    @@genres
+    @@genres.uniq
   end
   def self.genre_count
   end
